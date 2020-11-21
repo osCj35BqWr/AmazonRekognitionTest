f7a4cb89345a130e40f44a8fa3970e16b96c5858
osCj35BqWr <35219434+osCj35BqWr@users.noreply.github.com>
2020-11-21 19:05:24 +0900

    load image from youtube

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 #from boto3.session import Session
 import boto3
+import cv2
 from PIL import Image,ImageDraw
+import pafy # YouTube video capture
 
-inputPath = r".\img\test.jpeg"
-outputPath = r".\img\test_output.jpeg"
+# inputPath = r".\img\test.jpeg"
+inputPath = r".\img\input.jpeg"
+outputPath = r".\img\output.jpeg"
 
-# Rekognitionのクライアントを作成
-# IAMユーザーに割り当てたポリシー
-# AmazonRekognitionFullAccess
-# profile名は環境編巣にAWS_DEFAULT_PROFILE, AWS_PROFILEとして定義した。
-#session = Session(profile_name='XXX_profileName_XXX', region_name='ap-northeast-1')
-#client = session.client('rekognition')
+#渋谷スクランブル交差点
+#videoURL = "https://www.youtube.com/watch?v=lkIJYc4UH60"
+videoURL = "https://www.youtube.com/watch?v=UuTy56M29qs"
+video_pafy = pafy.new(videoURL)
+video_from_url = video_pafy.getbest().url
+cap = cv2.VideoCapture(video_from_url)
+ret, frame = cap.read()
+cv2.imwrite(inputPath,frame)
+cap.release()
+
+# Rekognitionのラベル検出を呼び出す
+# 【AWS IAM関連情報】
+#  IAMユーザーに割り当てたポリシー
+#   AmazonRekognitionFullAccess
+#  profile名
+#   環境編巣にAWS_DEFAULT_PROFILE, AWS_PROFILEとして定義した。
 client = boto3.client('rekognition', region_name='ap-northeast-1')
-
-# 画像ファイルを引数としてdetect_facesを実行
 with open(inputPath,'rb') as image:
-    #response = client.detect_faces(Image={'Bytes':image.read()},Attributes=['ALL'])
-    # MaxLabels=20はなくても良い
-    response = client.detect_labels(Image={'Bytes': image.read()}, MaxLabels=20)
+    response = client.detect_labels(Image={'Bytes': image.read()})
+
+#人数をカウントする
+cnt = 0
+
 # 物体が認識されない場合は処理終了
 if len(response['Labels'])==0:
     print('物体は認識されませんでした。')
@@ -27,11 +40,15 @@ else:
     imgWidth,imgHeight = img.size
     draw = ImageDraw.Draw(img)
 
-    # 認識された物体の数分、矩形セット処理を行う
-    for labels in response['Labels']:
-        print("Label: " + labels["Name"])
-        print("Confidence: " + str(labels["Confidence"]))
-        for instance in labels["Instances"]:
+    # 認識された物体のうち人間に該当するラベルについて、矩形セット処理を行う
+    for label in response['Labels']:
+        # 人間以外は除外する
+        if label['Name'] not in ['People', 'Person', 'Human']:
+            continue
+        for instance in label["Instances"]:
+
+            cnt += 1
+
             # BoundingBoxから物体の位置・サイズ情報を取得
             box = instance['BoundingBox']
             left = imgWidth * box['Left']
@@ -53,3 +70,6 @@ else:
 
     # 画像ファイルを表示
     img.show()
+
+    # 人数を表示する
+    print('検出された人数: {} 人'.format(cnt))
